07b3edc1ebc9199b1e46a49567c010b4281bc1cd
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2023-01-09 15:59:19 -0300

    consulta

--- src/dao/LoginDAO.java
+++ src/dao/LoginDAO.java
@@ -6,6 +6,8 @@ package dao;
 import beans.Login;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 import trabalhobd.conexao.Conexao;
 
 /**
@@ -32,6 +34,27 @@ public class LoginDAO {
        catch(Exception e){
            System.out.println("Erro ao inserir login: " +e);
        }
-       
+   }
+   
+   public Login getLogin(int id){
+       String sql = "SELECT * FROM login WHERE id = ?";
+       try {
+            PreparedStatement stmt = this.conn.prepareStatement(sql) ;
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            Login login = new Login();
+//            Primeiramente posiciona o rs na primeria posição
+            rs.first();
+            login.setId(id);
+            login.setNome(rs.getString("nome"));
+            login.setSenha(rs.getString("senha"));
+            login.setFrase(rs.getString("frase"));
+            JOptionPane.showMessageDialog(null, "Sucesso!");
+            return login;
+       } catch (Exception e) {
+           System.out.println("Erro ao consultar login: " +e);
+           JOptionPane.showMessageDialog(null, "Erro!");
+       }
+       return null;
    }
 }
